52ed7796989c399a870554ac453df8ff86e1388a
Berk Gokberk <gokberkb@mef.edu.tr>
2019-02-03 18:17:38 +0300

    Update Lab01.java

--- Lab 1 - Methods and Arrays/solution/Lab01.java
+++ Lab 1 - Methods and Arrays/solution/Lab01.java
@@ -6,6 +6,10 @@ public class Lab01 {
 	public static void main(String[] args) {
 		
 		double[] prices = {30.4, 100.6, 80.6, 40.4, 70.8, 66.8, 10.2, 2.6, 102.2, 90.0, 50.3, 21.6};
+		//double[] prices = {30.4, 100.6, 80.6, 40.4, 70.8, 66.8, 10.2, 2.6, 102.2, 90.0, 50.3, 21.6};
+		//double[] prices = {30.4, 100.6, 80.6, 40.4, 70.8, 66.8, 10.2, 2.6, 102.2, 90.0, 50.3, 21.6};
+		//double[] prices = {30.4, 100.6, 80.6, 40.4, 70.8, 66.8, 10.2, 2.6, 102.2, 90.0, 50.3, 21.6};
+		//double[] prices = {30.4, 100.6, 80.6, 40.4, 70.8, 66.8, 10.2, 2.6, 102.2, 90.0, 50.3, 21.6};
 		
 		double discountRate = 0.2; // make 20 percent discount 
 		double[] discountedPrices = makeDiscount(prices, discountRate);
