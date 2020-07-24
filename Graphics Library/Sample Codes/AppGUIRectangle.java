3164f22a677c4f608b0ef5a92fb0e47f505a4686
vanderberk <gokberk@gmail.com>
2020-07-24 15:59:29 +0300

    COMP110

--- Graphics Library/Sample Codes/AppGUIRectangle.java
+++ Graphics Library/Sample Codes/AppGUIRectangle.java
@@ -5,7 +5,14 @@
  * @author BG
  */
 public class AppGUIRectangle {
-	public static void main(String[] args) { 		
+	public static void main(String[] args) { 
+		
+		int canvas_width = 400; // specify canvas width 
+		int canvas_height = 400; // specify canvas height 
+		StdDraw.setCanvasSize(canvas_width, canvas_height);
+		StdDraw.setXscale(0, 1); // x axis scale is in the range [0,1]
+		StdDraw.setYscale(0, 1); // y axis scale is in the range [0,1]
+		
 		StdDraw.setPenColor(StdDraw.PRINCETON_ORANGE);
 		StdDraw.filledRectangle(0.5, 0.3, 0.15, 0.25);
 		
