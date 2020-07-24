02db4fe5209b77f1483f355f1928dca4249ee81c
vanderberk <gokberk@gmail.com>
2020-07-24 15:59:44 +0300

    COMP110

--- Graphics Library/Sample Codes/AppGUIText.java
+++ Graphics Library/Sample Codes/AppGUIText.java
@@ -8,6 +8,14 @@ import java.awt.Font;
  */
 public class AppGUIText {
 	public static void main(String[] args) {
+		
+		int canvas_width = 600; // specify canvas width 
+		int canvas_height = 400; // specify canvas height 
+		StdDraw.setCanvasSize(canvas_width, canvas_height);
+		StdDraw.setXscale(0, 1); // x axis scale is in the range [0,1]
+		StdDraw.setYscale(0, 1); // y axis scale is in the range [0,1]
+		
+		
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.setFont( new Font("Serif", Font.PLAIN, 32) );
 		StdDraw.textLeft(0.25, 0.4, "Font Type: Serif, Size: 32");
